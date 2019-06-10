605448ae30faee11cb1fd20909a1a0f84bd45d15
Garrett Young <garrettyoung510@gmail.com>
2019-06-10 13:38:26 -0700

    functions for capturing photos

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,90 +2,66 @@
 
 
 // *** Birthday Page Confirmation ***
+$(document).ready(function() {
 
-// on click function for submit
-$('#submit').on('click', function() {
-    // variable for user inputted date
-    var date = $('#picker').val();
-    console.log("date.valueOF(): " + date.valueOf());
-    console.log("moment of: " + moment(date).valueOf())
+    // on click function for submit
+    $('#submit').on('click', function() {
+        // variable for user inputted date
+        var date = $('#picker').val();
+        console.log("date.valueOF(): " + date.valueOf());
+        console.log("moment of: " + moment(date).valueOf())
 
-    // chosen date to unix
-    var chosenDate = moment(date).valueOf()
+        // chosen date to unix
+        var chosenDate = moment(date).valueOf()
 
-    // variable for todays date
-    var today = parseInt(moment().valueOf());
-    console.log("today: " + today);
+        // variable for todays date
+        var today = parseInt(moment().valueOf());
+        console.log("today: " + today);
 
-    // Getting age by subracting today from chosen date
-    console.log("Age: " + parseInt(today - chosenDate));
+        // Getting age by subracting today from chosen date
+        console.log("Age: " + parseInt(today - chosenDate));
 
-    var ofAge = parseInt(today - chosenDate)
-        // if statement for date entered
-    if (ofAge >= 662256000000) {
-        // if 21+ proceed to next page
-        console.log("Let's Drink!")
-    } else {
-        // else denied!
-        console.log("NOPE! Rejected");
-    }
-})
+        var ofAge = parseInt(today - chosenDate)
+            // if statement for date entered
+        if (ofAge >= 662256000000) {
+            // if 21+ proceed to next page
+            console.log("Let's Drink!")
+        } else {
+            // else denied!
+            console.log("NOPE! Rejected");
+        }
+    })
 
-
+});
 
 // *** Description of Service & Slider page ***
+$(document).ready(function() {
+    // if statement for acceptance of camera use
 
-// if statement for acceptance of camera use
+    // if accepted can proceed to evaluation & drink recommendation page after inputtnig slider info
 
-// if accepted can proceed to evaluation & drink recommendation page after inputtnig slider info
+    // else will proceed to drink recommendation page after slider page instead of face ++
 
-// else will proceed to drink recommendation page after slider page instead of face ++
-
+});
 
 // *** Evaluation & Drink Recommendation page ***
-
 // object for 50 different drink options
 
 //Check if browser supports camera use
 // const supported = 'mediaDevices' in navigator;
 
-
-// take photo and store in variable
-// const player = $('#player');
-// const canvas = $('#canvas');
-// const context = canvas.getContext('2d');
-// const captureButton = $('#capture');
-
-// const constraints = {
-//     video: true,
-// };
-
-// captureButton.on('click', () => {
-//     console.log("clicked!");
-//     // Draw the video frame to the canvas.
-//     context.drawImage(player, 0, 0, canvas.width, canvas.height);
-//     // Stop all video streams.
-//     player.srcObject.getVideoTracks().forEach(track => track.stop());
-// });
-
-// navigator.mediaDevices.getUserMedia(constraints)
-//     .then((stream) => {
-//         // Attach the video stream to the video element and autoplay.
-//         player.srcObject = stream;
-//     });
-
 const constraints = { "video": { width: { exact: 320 } } };
-var videoTag = $('#video-tag');
-var imageTag = $('#image-tag');
-var zoomSlider = $("#zoom-slider");
-var zoomSliderValue = $("#zoom-slider-value");
+var videoTag = document.getElementById('video-tag');
+var imageTag = document.getElementById('image-tag');
+var zoomSlider = document.getElementById("zoom-slider");
+var zoomSliderValue = document.getElementById("zoom-slider-value");
 var imageCapturer;
 
-function start() {
+$('#start').on('click', start() => {
     navigator.mediaDevices.getUserMedia(constraints)
         .then(gotMedia)
         .catch(e => { console.error('getUserMedia() failed: ', e); });
-}
+})
 
 function gotMedia(mediastream) {
     videoTag.srcObject = mediastream;
@@ -117,59 +93,58 @@ function gotMedia(mediastream) {
 
 }
 
-function takePhoto() {
-    imageCapturer.takePhoto()
-        .then((blob) => {
-            console.log("Photo taken: " + blob.type + ", " + blob.size + "B")
-            imageTag.src = URL.createObjectURL(blob);
-        })
-        .catch((err) => {
-            console.error("takePhoto() failed: ", e);
+$('#takephoto').on('click', takePhoto() => {
+            imageCapturer.takePhoto()
+                .then((blob) => {
+                    console.log("Photo taken: " + blob.type + ", " + blob.size + "B")
+                    imageTag.src = URL.createObjectURL(blob);
+                })
+                .catch((err) => {
+                    console.error("takePhoto() failed: ", e);
+                });
+        }
+
+
+
+        // run through face ++ api
+
+        // variable for query URL
+
+        // AJAX call using query url and photo
+        var queryURL = `https://api-us.faceplusplus.com/facepp/v3/face/analyze&api_key=XtvBZyeUXRy0uOEtl1mlG61af7JzBlIj&api_secret=tYNC1LAnUmHhUw_1IXhLLyKJXcZRHuvo`
+
+        // then response function
+
+        // pull user slider input from previous page
+
+        // pull face ++ results
+
+        // compare face ++ results & slider results
+
+        // SECONDARY - pull weather from weather api for the decision
+
+        // if statement with accepted camera permissions
+
+        // if accepted, use face ++ & weather to come up with drink recommendation
+
+
+        // else give drink recommendation based on slider
+
+        // DISPLAY 
+
+        var queryURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinks}`;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(`Drinks Data: ${response}`);
         });
-}
 
+        // picture of drink in the html
+        // ingredients
+        // instructions
 
+        // *** About Us & Contact Page ***
 
-// run through face ++ api
-
-// variable for query URL
-
-// AJAX call using query url and photo
-var queryURL = `https://api-us.faceplusplus.com/facepp/v3/face/analyze&api_key=XtvBZyeUXRy0uOEtl1mlG61af7JzBlIj&api_secret=tYNC1LAnUmHhUw_1IXhLLyKJXcZRHuvo`
-
-// then response function
-
-// pull user slider input from previous page
-
-// pull face ++ results
-
-// compare face ++ results & slider results
-
-// SECONDARY - pull weather from weather api for the decision
-
-// if statement with accepted camera permissions
-
-// if accepted, use face ++ & weather to come up with drink recommendation
-
-
-// else give drink recommendation based on slider
-
-// DISPLAY 
-
-var queryURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinks}`;
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(`Drinks Data: ${response}`);
-});
-
-// picture of drink in the html
-// ingredients
-// instructions
-
-
-// *** About Us & Contact Page ***
-
-// Contact us store the user inputs in a database?
+        // Contact us store the user inputs in a database?
